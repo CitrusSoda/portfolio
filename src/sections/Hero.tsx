7867f70ee44c9a6dd63fe45e2d93ea2d6dc1a4c3
CitrusSoda <dndhkaus1004@naver.com>
2024-11-12 16:43:18 +0900

    tutorial: leva 사용법

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,8 +3,47 @@ import { PerspectiveCamera } from '@react-three/drei';
 import HackerRoom from '../components/HackerRoom';
 import CanvasLoader from '../components/CanvasLoader';
 import { Suspense } from 'react';
+import { Leva, useControls } from 'leva';
 
 const Hero = () => {
+  const controls = useControls('HackerRoom', {
+    positionX: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionY: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionZ: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    rotationX: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationY: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationZ: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    scale: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+    },
+  });
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -15,13 +54,22 @@ const Hero = () => {
       </div>
       {/* three js */}
       <div className="size-full absolute inset-0">
+        <Leva />
         <Canvas className="size-full">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
             <HackerRoom
-              scale={0.05}
-              position={[0, 0, 0]}
-              rotation={[0, -Math.PI / 2, 0]}
+              position={[
+                controls.positionX,
+                controls.positionY,
+                controls.positionZ,
+              ]}
+              rotation={[
+                controls.rotationX,
+                controls.rotationY,
+                controls.rotationZ,
+              ]}
+              scale={[controls.scale, controls.scale, controls.scale]}
             />
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
